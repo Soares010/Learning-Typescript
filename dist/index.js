12dde6816fbdace1c62187db33aa89eb9b411cad
Mário Fernando Gomes Neto <mariosoares20012005@gmail.com>
2026-07-26 13:01:36 +0100

    Aprendendo sobre tipagem de variáveis com typescript

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Decalarando uma variável em typescript
+let age = 5;
+console.log(age);
+//# sourceMappingURL=index.js.map
